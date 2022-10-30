bed50f0da18d61336b505d9481a4fe714da851a4
Matt Gehrls <mattgehrls@gmail.com>
2022-10-30 15:57:38 -0400

    basic todos added, needs cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Audio from '../components/Audio'
 import Time from '../components/Time'
+import Todos from '../components/Todos'
 import Weather from '../components/Weather'
 
 const backgroundOptions = ["./hexspinner.gif", "./rain2.gif", "./cityskyline.gif"]
@@ -10,6 +11,7 @@ export default async function Home() {
 
   return (
     <div style={{position:"relative", display:"grid", placeContent:"center", height:"100vh", width:"100vw", backgroundImage:`url(${background})`, backgroundSize:'cover'}}>
+      <Todos/>
       <Time />
       <Weather/>
       <Audio />
---
+++ components/Todos.tsx
@@ -0,0 +1,105 @@
+"use client"
+
+import { useState} from 'react'
+import { nanoid } from 'nanoid'
+
+type Todo = {
+    id: string,
+    text: string,
+    done: boolean,
+}
+
+const Todos = () => {
+
+    const [todos, setTodos] = useState([] as Todo[])
+
+    function addTodo(){
+        let newTodo = {text:"", done:false, id:nanoid()}
+        setTodos([...todos, newTodo])
+    }
+    function removeTodo(id: string){
+        let newState = todos.filter(item => item.id !== id)
+        setTodos(newState)
+    }
+    function updateTodoText(e: React.ChangeEvent<HTMLInputElement>, id: string){
+        let newState = todos.map(todo =>{
+            if(todo.id === e.target.id){
+                return {...todo, text:e.target.value}
+            } else{
+                return todo
+            }
+        })
+        setTodos(newState)
+    }
+
+
+
+    const todoList = todos.map(item =>{
+        const todoProps ={
+            updateText: updateTodoText,
+            remove: removeTodo,
+            key: item.id,
+            id: item.id,
+            text: item.text,
+            addTodo: addTodo
+        }
+
+        return (
+            <Todo {...todoProps} />
+        )
+    })
+
+    return(
+        <div className='todos-container'>
+            <div className='todos-controls'>
+                <div className="todos-add-btn" onClick={()=>addTodo()}>
+                    Add Item
+                </div>
+            </div>
+            <p className='todos-title'>Todo List</p>
+            <div className='todo-list-container'>
+                {todoList}
+            </div>
+        </div>
+    )
+}
+interface TodoPropTypes {
+    updateText: (e: React.ChangeEvent<HTMLInputElement>, id: string) => void;
+    remove: (id: string) => void;
+    key: string;
+    id: string;
+    text: string;
+    addTodo: () => void;
+}
+
+const Todo = ({updateText, remove, id, text, addTodo}: TodoPropTypes) => {
+
+    let classes = "fa-regular fa-square-check transparent"
+    let hoveredClassed = "fa-regular fa-square-check"
+
+    return (
+        <div className="todo-container">
+            <input 
+                autoFocus
+                onChange={(e)=> updateText(e, id)} 
+                onKeyDown={(e)=>{
+                    if(e.key==="Enter"){
+                        addTodo()
+                    }
+                }} 
+                onFocus={(e)=>{
+                const end = text.length
+                e.target.setSelectionRange(end, end)
+                e.target.focus()
+                }} 
+                className="todo-text-box" 
+                id={id} type="text" 
+                value={text} />
+            <div className="remove-todo-icon">
+                <i onClick={()=> remove(id)} className={classes} />
+            </div>
+        </div>
+    )
+}
+
+export default Todos
